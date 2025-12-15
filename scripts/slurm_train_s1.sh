114e09094771679271b83db6e973b6ede840ad62
Tim Schneider <mail@tim-schneider.me>
2025-12-16 00:52:03 +0100

    Added slurm script

---
+++ scripts/slurm_train_s1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=hora_s1
+#SBATCH --output=../logs/%j_%x.txt
+#SBATCH --error=../logs/%j_%x.txt
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=16G
+#SBATCH --time=3-00:00:00
+#SBATCH --partition=gpu
+#SBATCH -C rtx&vram24gb
+
+cd ..
+conda run -n hora python train.py task=AllegroHandHora headless=True seed=0 \
+  task.env.forceScale=2 task.env.randomForceProbScalar=0.25 \
+  train.algo=PPO \
+  task.env.object.type=cylinder_default \
+  train.ppo.priv_info=True train.ppo.proprio_adapt=False \
+  train.ppo.output_name=AllegroHandHora/hora
